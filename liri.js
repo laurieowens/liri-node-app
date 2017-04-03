e355d1f78dc0150b8f288fd7d0c73e626d5412a6
Jake Owens <Jowens@Jakes-Air-3.attlocal.net>
2017-04-02 22:08:40 -0400

    sunday 2

--- liri.js
+++ liri.js
@@ -2,38 +2,55 @@ var Twitter = require('twitter');
 var Spotify = require('spotify');
 var request = require('request');
 var keys = require('./keys');
-
-var client = new Twitter({
-    consumer_key: keys.twitterKeys.consumer_key,
-    consumer_secret: keys.twitterKeys.consumer_secret,
-    access_token_key: keys.twitterKeys.access_token_key,
-    access_token_secret: keys.twitterKeys.access_token_secret
-});
+var nodeArgs1 = process.argv[2];
+var nodeArgs2 = process.argv[3];
 
 
+switch (nodeArgs1) {
+    case 'POTUS-tweets':
+        tweeter();
+        break;
+    case 'spotify-this-song':
+        spotifier(nodeArgs2);
+        break;
+    case 'movie-this':
+        moviegoer(nodeArgs2);
+        break;
+    case 'do-what-it-says':
+        break;
+    default:
+        tweeter();
 
-/**
- * Grab a list of favorited tweets
- **/
+}
 
-var params = ({
-    screen_name: 'POTUS',
-    count: '20'
-});
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        for (var i = 0; i < tweets.length; i++) {
-            console.log('%j \n', tweets[i].text, tweets[i].created_at);
+function tweeter() {
+    var client = new Twitter({
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret
+    });
+    /**
+     * Grab a list of favorited tweets
+     **/
+    var params = ({
+        screen_name: 'POTUS',
+        count: '20'
+    });
 
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log('%j \n', tweets[i].text, tweets[i].created_at);
+            }
         }
-    }
-});
+    });
+}
 
+function moviegoer() {
+    console.log('moviegoer');
+}
 
-
-
-// client.get('statuses/usertimeline', function(error, tweets, response) {
-//     if (!error) {
-//         console.log(tweets);
-//     }
-// });
+function spotifier() {
+    console.log('spotifier');
+}
